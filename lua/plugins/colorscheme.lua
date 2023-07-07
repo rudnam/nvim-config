f083eeb25c76d044d2c8743653133c1cf2e41cf2
rudnam <francisalbarracin2@gmail.com>
2023-07-08 00:14:45 +0800

    Update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,10 @@ return {
     require("github-theme").setup({})
 
     vim.cmd("autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE")
+    vim.cmd("autocmd ColorScheme * highlight NonText ctermbg=NONE guibg=NONE")
+    vim.cmd("autocmd ColorScheme * highlight NormalNC ctermbg=NONE guibg=NONE")
+    vim.cmd("autocmd ColorScheme * highlight NeoTreeNormal ctermbg=NONE guibg=NONE")
+    vim.cmd("autocmd ColorScheme * highlight NeoTreeNormalNC ctermbg=NONE guibg=NONE")
     vim.cmd("colorscheme github_dark_colorblind")
   end,
 }
