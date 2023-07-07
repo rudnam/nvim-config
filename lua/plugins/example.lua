68d74fc3f109bc3d065fc3b18f484be88c9987df
rudnam <francisalbarracin2@gmail.com>
2023-07-07 20:07:39 +0800

    Clean up

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -10,29 +10,6 @@ if true then return {} end
 -- * override the configuration of LazyVim plugins
 return {
 
-  {
-    "projekt0n/github-nvim-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("github-theme").setup({
-        -- ...
-      })
-
-      vim.cmd("colorscheme github_dark")
-    end,
-  },
-  -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
-  --
-  -- -- Configure LazyVim to load gruvbox
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "gruvbox",
-  --   },
-  -- },
-
   -- change trouble config
   {
     "folke/trouble.nvim",
