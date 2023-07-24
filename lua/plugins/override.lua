42a74120e59868a02ce7c02bc5f93cc22965eeaf
rudnam <francisalbarracin2@gmail.com>
2023-07-24 13:55:51 +0800

    Suppress "No information available" on hover"

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -77,5 +77,18 @@ return {
         },
       },
     },
+    -- Silence "No information available" on hover
+    {
+      "folke/noice.nvim",
+      opts = function(_, opts)
+        table.insert(opts.routes, {
+          filter = {
+            event = "notify",
+            find = "No information available",
+          },
+          opts = { skip = true },
+        })
+      end,
+    },
   },
 }
