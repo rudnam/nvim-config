0a2b884264ca2ede1a8efc2976433084bfc7bed4
rudnam <francisalbarracin2@gmail.com>
2023-07-31 09:09:35 +0800

    Disable transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,11 +5,11 @@ return {
   config = function()
     require("github-theme").setup({})
 
-    vim.cmd("autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE")
-    vim.cmd("autocmd ColorScheme * highlight NonText ctermbg=NONE guibg=NONE")
-    vim.cmd("autocmd ColorScheme * highlight NormalNC ctermbg=NONE guibg=NONE")
-    vim.cmd("autocmd ColorScheme * highlight NeoTreeNormal ctermbg=NONE guibg=NONE")
-    vim.cmd("autocmd ColorScheme * highlight NeoTreeNormalNC ctermbg=NONE guibg=NONE")
+    -- vim.cmd("autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE")
+    -- vim.cmd("autocmd ColorScheme * highlight NonText ctermbg=NONE guibg=NONE")
+    -- vim.cmd("autocmd ColorScheme * highlight NormalNC ctermbg=NONE guibg=NONE")
+    -- vim.cmd("autocmd ColorScheme * highlight NeoTreeNormal ctermbg=NONE guibg=NONE")
+    -- vim.cmd("autocmd ColorScheme * highlight NeoTreeNormalNC ctermbg=NONE guibg=NONE")
     vim.cmd("colorscheme github_dark_colorblind")
   end,
 }
