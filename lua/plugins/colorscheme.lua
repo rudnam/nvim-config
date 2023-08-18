c5b9f33475cc376e853c053d48ec6baeccfb3000
rudnam <francisalbarracin2@gmail.com>
2023-08-18 09:43:40 +0800

    Change theme to ayu dark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,10 @@
 return {
-  "projekt0n/github-nvim-theme",
+  "ayu-theme/ayu-vim",
   lazy = false,
   priority = 1000,
   config = function()
-    require("github-theme").setup({})
-
-    -- vim.cmd("autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE")
-    -- vim.cmd("autocmd ColorScheme * highlight NonText ctermbg=NONE guibg=NONE")
-    -- vim.cmd("autocmd ColorScheme * highlight NormalNC ctermbg=NONE guibg=NONE")
-    -- vim.cmd("autocmd ColorScheme * highlight NeoTreeNormal ctermbg=NONE guibg=NONE")
-    -- vim.cmd("autocmd ColorScheme * highlight NeoTreeNormalNC ctermbg=NONE guibg=NONE")
-    vim.cmd("colorscheme github_dark_colorblind")
+    vim.cmd("set termguicolors")
+    vim.cmd("let ayucolor='dark'")
+    vim.cmd("colorscheme ayu")
   end,
 }
